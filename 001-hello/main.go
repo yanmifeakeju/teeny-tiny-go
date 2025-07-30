9ca19f413c2b5f27d7f8e1f473adfb79a919a681
Oluwayanmife Akeju <yanmifeakeju@gmail.com>
2025-07-30 04:25:28 +0100

    initial commit

---
+++ 001-hello/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) >= 2 {
+		name := strings.Join(os.Args[1:], " ")
+		fmt.Printf("Hello, %s!\n", name)
+		return
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Enter a name (or 'quit' to exit): ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+
+		if input == "" {
+			fmt.Println("Hmmm..., I didn't quite catch the name")
+			continue
+		}
+
+		if strings.ToLower(input) == "quit" || strings.ToLower(input) == "exit" {
+			fmt.Println("Goodbye! ")
+			break
+		}
+
+		fmt.Printf("Hello, %s!\n", input)
+
+		fmt.Print("Continue? (y to continue, any other key to exit): ")
+		if scanner.Scan() {
+			response := strings.ToLower(strings.TrimSpace(scanner.Text()))
+			if response != "y" && response != "yes" {
+				fmt.Println("Goodbye!")
+				break
+			}
+		}
+
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# teeny-tiny-go
+
+# Teeny Tiny Go
+
+A repository of small and simple Go programs created while learning the language.
